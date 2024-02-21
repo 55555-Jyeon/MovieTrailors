70c2de735bd549223763147f6b49f291d9d42c51
55555-Jyeon <134191817+55555-Jyeon@users.noreply.github.com>
2024-02-21 22:38:59 +0900

    fetch api

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,20 @@
-export default function MovieDetail({
+import { Suspense } from "react";
+import MovieInfo from "../../../components/movie-info";
+import MovieVideo from "../../../components/movie-videos";
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <div>clicked movie : {id}</div>;
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info...</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie video...</h1>}>
+        <MovieVideo id={id} />
+      </Suspense>
+    </div>
+  );
 }
