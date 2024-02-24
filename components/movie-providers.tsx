e3aaceb0f7bf90e4d7ebcd2566c3851a0b28794a
55555-Jyeon <134191817+55555-Jyeon@users.noreply.github.com>
2024-02-24 16:38:38 +0900

    feat : movie-providers

--- components/movie-providers.tsx
+++ components/movie-providers.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/const";
+import styles from "../styles/movie-providers.module.css";
 
 async function getMovieProviders(id: string) {
   const response = await fetch(`${API_URL}/${id}/providers`);
@@ -7,6 +8,44 @@ async function getMovieProviders(id: string) {
 
 export default async function MovieProviders({ id }: { id: string }) {
   const providers = await getMovieProviders(id);
-  console.log(providers);
-  return <div>providers</div>;
+  console.log("providers", providers);
+  if (providers && typeof providers === "object") {
+    return (
+      <div className={styles.container}>
+        {Object.keys(providers).map((providerKey) => {
+          const provider = providers[providerKey];
+          return (
+            <div className={styles.country}>
+              <h1>{providerKey}</h1>
+              <div key={providerKey} className={styles.provider}>
+                <h3>{provider.provider_name}</h3>
+                {/* Rendering 'buy' array */}
+                {provider.buy && Array.isArray(provider.buy) && (
+                  <div className={styles.wrapper}>
+                    <h4>Buy</h4>
+                    {provider.buy.map((item, index) => (
+                      <div key={index} className={styles.method}>
+                        <img src={item.logo_path} alt={`Logo ${index}`} />
+                      </div>
+                    ))}
+                  </div>
+                )}
+                {/* Rendering 'rent' array */}
+                {provider.rent && Array.isArray(provider.rent) && (
+                  <div className={styles.wrapper}>
+                    <h4>Rent</h4>
+                    {provider.rent.map((item, index) => (
+                      <div key={index} className={styles.method}>
+                        <img src={item.logo_path} alt={`Logo ${index}`} />
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
