55d1695b63f03ad86ae4478ee22f696f71bc2a0e
55555-Jyeon <134191817+55555-Jyeon@users.noreply.github.com>
2024-02-24 17:29:18 +0900

    feat : error page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,15 @@
 import { Metadata } from "next";
+import styles from "../styles/error.module.css";
 
 export const metadata: Metadata = {
   title: "Not Found by learning how to dynamic dynamic",
 };
 
 export default function NotFound() {
-  return <div>custom not found</div>;
+  return (
+    <div className={styles.container}>
+      <img src="https://github.com/55555-Jyeon/startNext/assets/134191817/d9eb75ea-5935-4a1f-8596-16169263dc89" />
+      <p>Oops! Page not found!</p>
+    </div>
+  );
 }
