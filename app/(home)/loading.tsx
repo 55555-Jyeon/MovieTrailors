b8c5a63ad5cd03f50b23de9760b8a2db0e30284a
55555-Jyeon <134191817+55555-Jyeon@users.noreply.github.com>
2024-02-24 17:11:08 +0900

    feat : loading gif

--- app/(home)/loading.tsx
+++ app/(home)/loading.tsx
@@ -1,3 +1,9 @@
+import styles from "../../styles/loading.module.css";
+
 export default function Loading() {
-  return <h2>loading</h2>;
+  return (
+    <div className={styles.container}>
+      <img src="https://github.com/55555-Jyeon/startNext/assets/134191817/3b79ef68-ff39-41d6-87ec-5286f7295367" />
+    </div>
+  );
 }
